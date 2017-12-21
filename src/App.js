93644bb2d8a3b4d42079d0f11fcb1d3b18b62539
dawsoncanby <dawsoncanby@gmail.com>
2017-12-21 15:01:28 -0700

    added header, recent tracks

--- src/App.js
+++ src/App.js
@@ -7,12 +7,15 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Supa Squidds</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+            <h2>Supa Squidds</h2>
+            </div>
+            <p className="App-intro">
+            A music group from Denver, Colorado
+            </p>
+            <div>
+                <h2>Recent Tracks</h2>
+                <iframe width="75%" height="450" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/users/229298628&amp;color=%23ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;show_teaser=true"></iframe>
+            </div>
       </div>
     );
   }
