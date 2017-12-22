d12942167d950790347aa26da69c3164cc728aa2
dawsoncanby <dawsoncanby@gmail.com>
2017-12-22 01:41:34 -0700

    changed layout and style, added info section

--- src/App.js
+++ src/App.js
@@ -6,16 +6,23 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-            <h2>Supa Squidds</h2>
-            </div>
-            <p className="App-intro">
-            A music group from Denver, Colorado
-            </p>
-            <div>
-                <h2>Recent Tracks</h2>
-                <iframe width="75%" height="450" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/users/229298628&amp;color=%23ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;show_teaser=true"></iframe>
-            </div>
+          <div className="App-header">
+              <h2>Supa Squidds</h2>
+              <h3>A music group from Denver, Colorado</h3>
+          </div>
+
+
+
+          <div className="App-content-section">
+              <h2>Info</h2>
+              <p className="App-intro">
+                  Inquiries: wilanddawsonbeats@gmail.com
+              </p>
+          </div>
+          <div className="App-content-section">
+            <h2>Recent Tracks</h2>
+            <iframe width="90%" height="450" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/users/229298628&amp;color=%23ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;show_teaser=true"></iframe>
+          </div>
       </div>
     );
   }
