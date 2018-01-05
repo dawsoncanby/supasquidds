e7995497203b4ef2f42438a01c24fe4e1758d511
dawsoncanby <dawsoncanby@gmail.com>
2018-01-04 17:51:59 -0700

    added links to soundcloud and youtube

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-          <div className="App-header">
+          <header className="App-header">
               <h2>Supa Squidds</h2>
-              <h3>A music group from Denver, Colorado</h3>
-          </div>
+              <h5>A music group from Denver, Colorado</h5>
+              <a>Listen On: </a>
+              <a href="https://soundcloud.com/will-and-dawson-beats">SoundCloud</a>
+              &nbsp;
+              <a href="https://www.youtube.com/channel/UCR9w3xFUSjoym2O0Qnqwiyg">YouTube</a>
+          </header>
 
-
-
-          <div className="App-content-section">
-              <h2>Info</h2>
-              <p className="App-intro">
-                  Inquiries: wilanddawsonbeats@gmail.com
-              </p>
-          </div>
           <div className="App-content-section">
             <h2>Recent Tracks</h2>
             <iframe width="90%" height="450" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/users/229298628&amp;color=%23ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;show_teaser=true"></iframe>
